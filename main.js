1e409e08e627891a25cd95c592875f6c0cfb11ba
MuniS67 <tursunovmunis34@gmail.com>
2023-11-25 15:57:56 +0500

    m

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 import { getMovieData } from "./modules/helpers";
-import { genre_types, header_create, reload_movies } from "./modules/ui";
+import {
+  genre_types,
+  header_create,
+  reload_movies,
+  reload_popular,
+} from "./modules/ui";
 
 let header = document.querySelector("header");
 let moviePlace = document.querySelector(".movies");
 let genresPlace = document.querySelector(".genres");
 let mainBg = document.querySelector(".mainBg");
 let btnShowAll = document.querySelector(".btn-show-all");
+let like_dislike = document.querySelectorAll(".sec1 .bottom .right div");
+let popularMovies = document.querySelector(".swiper-wrapper");
 
 header_create(header);
 
@@ -15,8 +22,37 @@ Promise.all([
 ]).then(([movies, genres]) => {
   console.log(movies.data.results);
   genre_types(genres.data.genres, genresPlace);
-  reload_movies(movies.data.results, moviePlace, genres.data.genres, mainBg);
-  btnShowAll.onclick = () => {
-    location.assign("./pages/showAllMovies/");
+  reload_movies(
+    movies.data.results,
+    moviePlace,
+    genres.data.genres,
+    mainBg,
+    true
+  );
+  reload_popular(
+    movies.data.results,
+    popularMovies,
+    genres.data.genres,
+    mainBg
+  );
+});
+
+btnShowAll.onclick = () => {
+  location.assign("./pages/showAllMovies/");
+};
+
+like_dislike.forEach((btn) => {
+  btn.onclick = () => {
+    like_dislike.forEach((button) => (button.style.background = "#1b2133"));
+    btn.style.background = "black";
   };
+});
+
+new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
--- modules/ui.js
+++ modules/ui.js
@@ -41,10 +41,152 @@ export function header_create(place) {
       </div>
       `;
 }
-
-export function reload_movies(arr, place, genres, bg) {
+export function reload_movies(arr, place, genres, bg, plite) {
   place.innerHTML = "";
-  for (let idx = 0; idx < 8; idx++) {
+  if (plite === false) {
+    for (let item of arr) {
+      let block = document.createElement("div");
+      let img = document.createElement("img");
+      let ratingBlock = document.createElement("div");
+      let rating = document.createElement("p");
+      let name = document.createElement("h4");
+      let genre = document.createElement("p");
+      let genreTitles = [];
+
+      for (let id of item.genre_ids) {
+        for (let genre of genres) {
+          if (id === genre.id) {
+            genreTitles.push(genre.name);
+          }
+        }
+      }
+
+      name.classList.add("title");
+      genre.classList.add("genre");
+      ratingBlock.classList.add("ratingBlock");
+      rating.classList.add("rating");
+
+      img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
+      name.innerHTML = item.title;
+      genre.innerHTML = genreTitles.join(", ");
+      rating.innerHTML = item.vote_average;
+
+      let hoverBg = document.createElement("div");
+      let hoverBtn = document.createElement("button");
+
+      hoverBtn.innerHTML = "Карточка фильма";
+      hoverBg.classList.add("hoverBlock");
+
+      block.onmouseenter = () => {
+        bg.style.backgroundImage =
+          "url(https://image.tmdb.org/t/p/original" + item.backdrop_path + ")";
+        hoverBg.classList.add("flex");
+      };
+      block.onmouseleave = () => {
+        hoverBg.classList.remove("flex");
+      };
+
+      img.onclick = () => {
+        console.log(click);
+        let iframe = document.querySelector("#mainFrame");
+        let id = item.id;
+        getMovieData(`/movie/${id}/videos`).then((res) =>
+          setTrailers(res.data.results[0], iframe)
+        );
+      };
+      hoverBtn.onclick = () => {
+        location.assign("../pages/aboutMovie/");
+      };
+
+      place.append(block);
+      block.append(img, hoverBg, ratingBlock, name, genre);
+      ratingBlock.append(rating);
+      hoverBg.append(hoverBtn);
+    }
+  } else {
+    for (let idx = 0; idx < 8; idx++) {
+      let block = document.createElement("div");
+      let img = document.createElement("img");
+      let ratingBlock = document.createElement("div");
+      let rating = document.createElement("p");
+      let name = document.createElement("h4");
+      let genre = document.createElement("p");
+      let genreTitles = [];
+
+      for (let id of arr[idx].genre_ids) {
+        for (let genre of genres) {
+          if (id === genre.id) {
+            genreTitles.push(genre.name);
+          }
+        }
+      }
+
+      name.classList.add("title");
+      genre.classList.add("genre");
+      ratingBlock.classList.add("ratingBlock");
+      rating.classList.add("rating");
+
+      img.src = "https://image.tmdb.org/t/p/original" + arr[idx].poster_path;
+      name.innerHTML = arr[idx].title;
+      genre.innerHTML = genreTitles.join(", ");
+      rating.innerHTML = arr[idx].vote_average;
+
+      let hoverBg = document.createElement("div");
+      let hoverBtn = document.createElement("button");
+
+      hoverBtn.innerHTML = "Карточка фильма";
+      hoverBg.classList.add("hoverBlock");
+
+      block.onmouseenter = () => {
+        bg.style.backgroundImage =
+          "url(https://image.tmdb.org/t/p/original" +
+          arr[idx].backdrop_path +
+          ")";
+        hoverBg.classList.add("flex");
+      };
+      block.onmouseleave = () => {
+        hoverBg.classList.remove("flex");
+      };
+
+      hoverBg.onclick = () => {
+        let iframe = document.querySelector("#mainFrame");
+        let title = document.querySelector(".sec1 .bottom .left .title");
+        let id = arr[idx].id;
+        getMovieData(`/movie/${id}/videos`).then((res) =>
+          setTrailers(res.data.results[0], iframe)
+        );
+        title.innerHTML = arr[idx].title;
+      };
+      hoverBtn.onclick = () => {
+        location.assign("../pages/aboutMovie/");
+      };
+
+      place.append(block);
+      block.append(img, hoverBg, ratingBlock, name, genre);
+      ratingBlock.append(rating);
+      hoverBg.append(hoverBtn);
+    }
+  }
+}
+
+export function genre_types(arr, place) {
+  place.innerHTML = "";
+  for (let idx = 0; idx < 5; idx++) {
+    let a = document.createElement("a");
+    let p = document.createElement("p");
+
+    a.href = "#";
+    p.innerHTML = arr[idx].name;
+
+    place.append(a);
+    a.append(p);
+  }
+}
+
+export function reload_popular(arr, place, genres, bg) {
+  place.innerHTML = "";
+  for (let idx = 0; idx < 15; idx++) {
+    let mainBlock = document.createElement("div");
     let block = document.createElement("div");
     let img = document.createElement("img");
     let ratingBlock = document.createElement("div");
@@ -65,6 +207,7 @@ export function reload_movies(arr, place, genres, bg) {
     genre.classList.add("genre");
     ratingBlock.classList.add("ratingBlock");
     rating.classList.add("rating");
+    mainBlock.classList.add("swiper-slide");
 
     img.src = "https://image.tmdb.org/t/p/original" + arr[idx].poster_path;
     name.innerHTML = arr[idx].title;
@@ -85,96 +228,24 @@ export function reload_movies(arr, place, genres, bg) {
       hoverBg.classList.add("flex");
     };
     block.onmouseleave = () => {
-      bg.style.backgroundImage = "none";
-      hoverBg.classList.remove("flex");
-    };
-
-    hoverBtn.onclick = () => {
-      let iframe = document.querySelector("#mainFrame");
-      let id = arr[idx].id;
-      getMovieData(`/movie/${id}/videos`).then((res) =>
-        setTrailers(res.data.results[0], iframe)
-      );
-    };
-
-    place.append(block);
-    block.append(img, hoverBg, ratingBlock, name, genre);
-    ratingBlock.append(rating);
-    hoverBg.append(hoverBtn);
-  }
-}
-
-export function genre_types(arr, place) {
-  place.innerHTML = "";
-  for (let idx = 0; idx < 5; idx++) {
-    let a = document.createElement("a");
-    let p = document.createElement("p");
-
-    a.href = "#";
-    p.innerHTML = arr[idx].name;
-
-    place.append(a);
-    a.append(p);
-  }
-}
-
-export function reload_movies_all(arr, place, genres, bg) {
-  place.innerHTML = "";
-  for (let item of arr) {
-    let block = document.createElement("div");
-    let img = document.createElement("img");
-    let ratingBlock = document.createElement("div");
-    let rating = document.createElement("p");
-    let name = document.createElement("h4");
-    let genre = document.createElement("p");
-    let genreTitles = [];
-
-    for (let id of item.genre_ids) {
-      for (let genre of genres) {
-        if (id === genre.id) {
-          genreTitles.push(genre.name);
-        }
-      }
-    }
-
-    name.classList.add("title");
-    genre.classList.add("genre");
-    ratingBlock.classList.add("ratingBlock");
-    rating.classList.add("rating");
-
-    img.src = "https://image.tmdb.org/t/p/original" + item.poster_path;
-    name.innerHTML = item.title;
-    genre.innerHTML = genreTitles.join(", ");
-    rating.innerHTML = item.vote_average;
-
-    let hoverBg = document.createElement("div");
-    let hoverBtn = document.createElement("button");
-
-    hoverBtn.innerHTML = "Карточка фильма";
-    hoverBg.classList.add("hoverBlock");
-
-    block.onmouseenter = () => {
-      bg.style.backgroundImage =
-        "url(https://image.tmdb.org/t/p/original" + item.backdrop_path + ")";
-      hoverBg.classList.add("flex");
-    };
-    block.onmouseleave = () => {
-      bg.style.backgroundImage = "none";
       hoverBg.classList.remove("flex");
     };
 
     hoverBg.onclick = () => {
       let iframe = document.querySelector("#mainFrame");
-      let id = item.id;
-      getMovieData(`/movie/${id}/videos?language=ru`).then((res) =>
+      let title = document.querySelector(".sec1 .bottom .left .title");
+      let id = arr[idx].id;
+      getMovieData(`/movie/${id}/videos`).then((res) =>
         setTrailers(res.data.results[0], iframe)
       );
+      title.innerHTML = arr[idx].title;
     };
     hoverBtn.onclick = () => {
       location.assign("../pages/aboutMovie/");
     };
 
-    place.append(block);
+    place.append(mainBlock);
+    mainBlock.append(block);
     block.append(img, hoverBg, ratingBlock, name, genre);
     ratingBlock.append(rating);
     hoverBg.append(hoverBtn);
